2e210e6cc744a7607d54f46300d7ec703d164b13
bradyshehu <shehu.brady@gmail.com>
2025-07-29 19:18:53 +0200

    added getters and setters

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -5,12 +5,23 @@ public class Main {
 
     Random rand = new Random();
     public static void main(String[] args) {
+
+
         Prodotto telefono = new Prodotto("Iphone", "Ultima generazione", 100.00f, 22);
 
-        System.out.println(telefono.nome);
+
+        System.out.println(telefono.getCodice());
+        System.out.println(telefono.getNome());
+        System.out.println(telefono.getDescrizione());
         System.out.printf("%.2f\n",telefono.getPrezzo());
+        System.out.println(telefono.getIva());
         System.out.printf("%.2f\n",telefono.getPrezzoFinale());
         System.out.println(telefono.getNomeCompleto());
+
+
+        telefono.setDescrizione("Aggiornato nuovamente articolo");
+        System.out.println(telefono.getDescrizione());
+
     }
     
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -6,12 +6,20 @@ public class Prodotto {
 
     Random rand = new Random();
 
-    int codice;
-    String nome;
-    String descrizione;
-    float prezzo;
-    int iva;
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private float prezzo;
+    private int iva;
     
+    public Prodotto (){
+        this.codice = rand.nextInt(1000);
+        this.nome = "";
+        this.descrizione = "";
+        this.prezzo = 0;
+        this.iva = 0;
+    };
+
     public Prodotto (String nome, String descrizione, float prezzo, int iva){
         this.codice = rand.nextInt(1000);
         this.nome = nome;
@@ -20,8 +28,22 @@ public class Prodotto {
         this.iva = iva;
     };
 
+    // GETTER
+
+    public int getCodice (){
+        return codice;
+    }
+    public String getNome (){
+        return nome;
+    }
+    public String getDescrizione (){
+        return descrizione;
+    }
     public float getPrezzo() {
         return prezzo;
+    }
+    public int getIva(){
+        return iva;
     }
     public float getPrezzoFinale() {
         float prezzoFinale = prezzo + (prezzo * iva / 100);
@@ -30,5 +52,31 @@ public class Prodotto {
     public String getNomeCompleto () {
         String nomeCompleto = codice + "-" + nome;
         return nomeCompleto;
+    }
+
+
+    // SETTER
+
+    public void setNome (String nome){
+        if (nome.length() > 0){
+            this.nome = nome;
+        }
+    }
+    public void setDescrizione (String descrizione){
+        if (descrizione.length() > 0){
+            this.descrizione = descrizione;
+        }
+    }
+    public void setPrezzo(float prezzo) {
+        if(prezzo >= 0)
+        {
+            this.prezzo=prezzo;
+        }
+    }
+    public void setIva(int iva) {
+        if(iva >= 0)
+        {
+            this.iva=iva;
+        }
     }
 }
